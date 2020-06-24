db6ae2a4d1160cb7433fe8f79f0a8ee0ec65247b
Lee_JunWon <cpprhtn@naver.com>
2020-06-24 21:59:07 +0900

    6/24

---
+++ R_study/R_study/UCBAdmissions.R
@@ -0,0 +1,13 @@
+addmargins(apply(UCBAdmissions, c(3,1), sum)) -> test
+test[1,1]/test[1,3] -> A_
+test[2,1]/test[2,3] -> B_
+test[3,1]/test[3,3] -> C_
+test[4,1]/test[4,3] -> D_
+test[5,1]/test[5,3] -> E_
+test[6,1]/test[6,3] -> F_
+A_
+B_
+C_
+D_
+E_
+F_
